032574bf85a48085322545918b0afe261909c549
İrem Doğan <65050139+iremdgn@users.noreply.github.com>
2021-11-08 16:27:14 +0300

    update

--- game.js
+++ game.js
@@ -1,8 +1,35 @@
+
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+var userClickedPattern = [];
+
+
+$(".btn").click(function() {
+
+   var userChosenColour = $(this).attr("id");
+
+   userClickedPattern.push(userChosenColour);
+
+   console.log(userClickedPattern);
+});
+
 function nextSequence() {
 
     var randomNumber = Math.floor(Math.random() * 4);
 
-    var buttonColors = [red, blue, green, yellow];
+    var randomChosenColour = buttonColors[randomNumber];
 
-    var randomChosenColour = buttonColors(randomNumber);
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
 }
+
+
+
+
+

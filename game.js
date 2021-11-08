dbc2d87fbced1c95971d6732ff05efa4ea27c451
İrem Doğan <65050139+iremdgn@users.noreply.github.com>
2021-11-08 15:03:04 +0300

    random colors select

---
+++ game.js
@@ -0,0 +1,8 @@
+function nextSequence() {
+
+    var randomNumber = Math.floor(Math.random() * 4);
+
+    var buttonColors = [red, blue, green, yellow];
+
+    var randomChosenColour = buttonColors(randomNumber);
+}

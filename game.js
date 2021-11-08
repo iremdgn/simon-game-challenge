eab9759595acd399e546149f5eaf7bcd75b30b5b
İrem Doğan <65050139+iremdgn@users.noreply.github.com>
2021-11-08 17:49:27 +0300

    sound and efect update

--- game.js
+++ game.js
@@ -12,7 +12,10 @@ $(".btn").click(function() {
 
    userClickedPattern.push(userChosenColour);
 
-   console.log(userClickedPattern);
+   playSound(userChosenColour);
+
+   animatePress(userChosenColour);
+
 });
 
 function nextSequence() {
@@ -25,11 +28,22 @@ function nextSequence() {
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
+    playSound(randomChosenColour);
+
 }
 
 
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
 
+function animatePress(currentColour) {
+    $("." + currentColour).addClass("pressed");
+
+    setTimeout(function() {
+        $("." + currentColour).removeClass("pressed");
+    }, 100);
+}
 
 

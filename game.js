5ebe3ca33739ef38c3daee47b541b68ccfc396e6
İrem Doğan <65050139+iremdgn@users.noreply.github.com>
2021-11-08 19:59:22 +0300

    start game update

--- game.js
+++ game.js
@@ -5,38 +5,40 @@ var gamePattern = [];
 
 var userClickedPattern = [];
 
+var start = false;
+
 var level = 0;
 
-$(document).keypress(function(e) {
-    nextSequence();
-
-    $("h1").text("Level "+ level);
+$(document).keypress(function() {
+    if(!start) {
+        $("#level-title").text("Level "+ level);
+        nextSequence();
+        start = true;
+    }
 });
 
 $(".btn").click(function() {
 
    var userChosenColour = $(this).attr("id");
-
    userClickedPattern.push(userChosenColour);
 
    playSound(userChosenColour);
-
    animatePress(userChosenColour);
 
 });
 
 function nextSequence() {
 
+    level++; 
+    $("#level-title").text("Level " + level);
+
     var randomNumber = Math.floor(Math.random() * 4);
-
     var randomChosenColour = buttonColors[randomNumber];
-
     gamePattern.push(randomChosenColour);
 
-    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);  
     playSound(randomChosenColour);
-
+  
 }
 
 
